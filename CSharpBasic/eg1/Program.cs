cedb19be0de791490c407b6039f03f19f723833b
DESKTOP-O0TS09B\Admin <nguyentiendatndh19@gmail.com>
2021-11-10 15:02:01 +0700

    no message

--- CSharpBasic/eg1/Program.cs
+++ CSharpBasic/eg1/Program.cs
@@ -6,7 +6,7 @@ namespace eg1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Kteam");
             Console.ReadKey();
         }
     }
