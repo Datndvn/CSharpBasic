cd06ce5a6d81563a906589d333c3fc64860b8521
DESKTOP-O0TS09B\Admin <nguyentiendatndh19@gmail.com>
2021-11-11 20:36:56 +0700

    Nhap_xuat_co_ban

---
+++ CSharpBasic/Nhap_xuat_co_ban/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Nhap_xuat_co_ban
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("       Kteam");
+            Console.Write(" Moi nhap ten cua minh: ");
+            Console.WriteLine(" Ten cua ban la: " + Console.ReadLine());
+            Console.Write(" Moi nhap ngay sinh: ");
+            Console.WriteLine(" Ngay sinh cua ban la: " + Console.ReadLine());
+            Console.Write(" Moi nhap que quan: ");
+            Console.WriteLine(" Que quan: " + Console.ReadLine());
+
+            Console.ReadKey();
+        }
+    }
+}
