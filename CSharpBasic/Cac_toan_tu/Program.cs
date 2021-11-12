d8210cb89772c528fedce3b626104756a1c82868
DESKTOP-O0TS09B\Admin <nguyentiendatndh19@gmail.com>
2021-11-12 20:22:46 +0700

    Cac_toan_tu

---
+++ CSharpBasic/Cac_toan_tu/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Cac_toan_tu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                string strSoNguyen;
+                int SoNguyen;
+                string KetQua;
+
+                strSoNguyen = Console.ReadLine();
+                SoNguyen = Int32.Parse(strSoNguyen);
+                KetQua = (SoNguyen % 2 == 0) ? "so chan" : "so le";
+            Console.WriteLine("{0} la {1}", SoNguyen, KetQua);
+
+            Console.ReadKey();
+
+        }
+    }
+}
