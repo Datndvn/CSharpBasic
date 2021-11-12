af8f91d0780aaf9f33c72af2e0ce1b981645454e
DESKTOP-O0TS09B\Admin <nguyentiendatndh19@gmail.com>
2021-11-12 19:24:41 +0700

    Kieu_du_lieu

---
+++ CSharpBasic/Kieu_du_lieu/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Kieu_du_lieu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte Bienbyte = 10;
+            short Bienshort = 10;
+            int Bienint = 10;
+            long Bienlong = 10;
+            float Bienfloat = 11.9f;
+            double Biendouble = 11.9;
+            decimal Biendecimal = 11.9m;
+            char Bienchar = 'M';
+            string Bienstring = "Hello";
+            Console.ReadKey();
+        }
+    }
+}
