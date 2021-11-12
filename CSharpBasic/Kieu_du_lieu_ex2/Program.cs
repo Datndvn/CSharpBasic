9fc09c2ea4205595f1237c19c5610699ada2c41e
DESKTOP-O0TS09B\Admin <nguyentiendatndh19@gmail.com>
2021-11-12 19:57:29 +0700

    Kieu_du_lieu_ex2

---
+++ CSharpBasic/Kieu_du_lieu_ex2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Kieu_du_lieu_ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                byte BienByte = 3;
+                short BienShort = 9;
+                int BienInt = 10;
+                long BienLong = 0;
+
+                BienLong = BienByte; 
+                Console.WriteLine(" BienLong = " + BienLong);
+                BienLong = BienInt;
+                Console.WriteLine(" BienLong = {0}", BienLong);
+                BienShort = BienByte;
+                Console.WriteLine(" BienShort = " + BienShort);
+                BienInt = BienShort;
+                Console.WriteLine(" BienInt = " + BienInt);
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
