59d851eca8b0a7e80fa9081b2fd5f29e75220687
DESKTOP-O0TS09B\Admin <nguyentiendatndh19@gmail.com>
2021-11-13 18:13:02 +0700

    ep_ kieu

---
+++ CSharpBasic/Ep_kieu/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Ep_kieu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B;
+            int Tong, Hieu, Tich;
+            double Thuong;
+            string strA, strB; 
+
+            Console.WriteLine("         **************************************************");
+            Console.WriteLine("         *                                         			       *");
+            Console.WriteLine("         *   Chuong trinh tinh tong, hieu, tich, thuong  *");
+            Console.WriteLine("         *                                            		       *");
+            Console.WriteLine("         **************************************************");
+
+            Console.Write("\n Moi ban nhap so A: ");
+            strA = Console.ReadLine();
+            Console.Write(" Moi ban nhap so B: ");
+            strB = Console.ReadLine();
+
+            A = int.Parse(strA);
+            B = int.Parse(strB);
+
+            Tong = A + B;
+            Hieu = A - B;
+            Tich = A * B;
+            Thuong = (double)A / B;
+
+            Console.WriteLine(" Tong = " + Tong);
+            Console.WriteLine(" Hieu = " + Hieu);
+            Console.WriteLine(" Tich = " + Tich);
+            Console.WriteLine(" Thuong = " + Thuong);
+
+        }
+    }
+}
