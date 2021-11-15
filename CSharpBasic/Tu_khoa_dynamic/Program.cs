6eefe2087ca0e13ee2d9dcc12f1204412bc762f0
DESKTOP-O0TS09B\Admin <nguyentiendatndh19@gmail.com>
2021-11-15 20:03:12 +0700

    Tu_khoa_dynamic

---
+++ CSharpBasic/Tu_khoa_dynamic/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Tu_khoa_dynamic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name = "HowKteam ";
+            string Mission = "Free Education";
+            dynamic DynamicName = Name;
+            Console.WriteLine("Mission of " + DynamicName + " is " + Mission);
+            Console.ReadKey();
+        }
+    }
+}
