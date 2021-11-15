7df1c0f39aaee603ff6b25c90a329b88e8312605
DESKTOP-O0TS09B\Admin <nguyentiendatndh19@gmail.com>
2021-11-15 19:52:58 +0700

    Keu_du_lieu_va_tu_khoa_var

---
+++ CSharpBasic/Kieu_du_lieu_Object/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Kieu_du_lieu_Object
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Object Obj = "Hello";
+            int Value = 109;
+            Object ObjectValue = Value;
+            int NewValue = (int)ObjectValue;
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CSharpBasic/Tu_khoa_var/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Tu_khoa_var
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var varString = "Hello";
+            string content = "Bye";
+            Console.WriteLine(varString);
+            Console.WriteLine(content);
+            Console.ReadKey();
+        }
+    }
+}
